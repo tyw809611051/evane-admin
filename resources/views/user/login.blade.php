9e81febe4060d279317e81c7ed932e01e1e7185c
tangyiwen <tangyiwen@cissdata.com>
2018-07-02 16:06:13 +0800

    login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -6,24 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--Plugin styles-->
     <link type="text/css" rel="stylesheet" href="css/app.css">
+    <style>
+        * { margin: 0; padding: 0; }
+        html { height: 100%; }
+        body { height: 100%; background: #fff url(img/bag.jpg) 50% 50% no-repeat; background-size: cover;}
+        .login_box { position: absolute; left: 50%; top: 50%; width: 430px; height: 550px; margin: -300px 0 0 -215px; border: 1px solid #fff; border-radius: 20px; overflow: hidden;}
+        .logo { width: 104px; height: 104px; margin: 50px auto 80px; background: url(img/login.png) 0 0 no-repeat; }
+    </style>
 </head>
 <body class="login_background">
 <div class="container wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
-    <div class="row align-items-center">
-        <div class="col">
-            <p>账号</p>
+    <div class="login_box"> 
+        <div class="logo">
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>账号</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>密码</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>登录</p>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            <p>密码</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <p>登录</p>
-        </div>
-    </div>
+
 </div>
 <!-- global js -->
 <script type="text/javascript" src="js/app.js"></script>
