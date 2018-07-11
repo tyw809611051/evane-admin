9e30af46fd86e5f411506b9c91493598aeee39e1
tangyiwen <tangyiwen@cissdata.com>
2018-07-11 14:32:18 +0800

    导航

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="content">
   <div class="container-fluid">
-    <div class="row">
+  <div class="row">
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card card-stats">
           <div class="card-header card-header-warning card-header-icon">
@@ -21,7 +21,41 @@
           </div>
         </div>
       </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+          <div class="card-header card-header-rose card-header-icon">
+            <div class="card-icon">
+              <i class="material-icons">equalizer</i>
+            </div>
+            <p class="card-category">Website Visits</p>
+            <h3 class="card-title">75.521</h3>
+          </div>
+          <div class="card-footer">
+            <div class="stats">
+              <i class="material-icons">local_offer</i> Tracked from Google Analytics
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+          <div class="card-header card-header-success card-header-icon">
+            <div class="card-icon">
+              <i class="material-icons">store</i>
+            </div>
+            <p class="card-category">Revenue</p>
+            <h3 class="card-title">$34,245</h3>
+          </div>
+          <div class="card-footer">
+            <div class="stats">
+              <i class="material-icons">date_range</i> Last 24 Hours
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+    
     <div class="row">
     </div>
     <div class="row">
@@ -59,6 +93,8 @@
                   </table>
                 </div>
               </div>
+
+              
               <div class="col-md-6 ml-auto mr-auto">
                 <div id="worldMap" style="height: 300px;"></div>
               </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,8 +59,8 @@
             <div class="user-info">
                 <a data-toggle="collapse" href="#collapseExample" class="username">
                     <span>
-                       Tania Andrew 
-                      <b class="caret"></b>
+                       静爷
+        
                     </span>
                 </a>
             </div>
@@ -69,59 +69,166 @@
             <li class="nav-item active ">
                 <a class="nav-link" href="../examples/dashboard.html">
                     <i class="material-icons">dashboard</i>
-                    <p> Dashboard </p>
+                    <p> 网站统计 </p>
                 </a>
             </li>
             
             <li class="nav-item ">
-                <a class="nav-link" data-toggle="collapse" href="#pagesExamples">
-                    <i class="material-icons">image</i>
-                    <p> Pages 
+                <a class="nav-link" data-toggle="collapse" href="#articleExamples">
+                    <i class="material-icons">book</i>
+                    <p> 文章管理 
+                    <b class="caret"></b>
                     </p>
                 </a>
 
-                <div class="collapse" id="pagesExamples">
+                <div class="collapse" id="articleExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 新 </span>
+                              <span class="sidebar-normal"> 新的文章 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 文章 </span>
+                              <span class="sidebar-normal"> 文章管理 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 分类 </span>
+                              <span class="sidebar-normal"> 分类管理 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 版块 </span>
+                              <span class="sidebar-normal"> 版块管理 </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" data-toggle="collapse" href="#commentExamples">
+                    <i class="material-icons">comment</i>
+                    <p> 评论管理 
+                    <b class="caret"></b>
+                    </p>
+                </a>
+
+                <div class="collapse" id="commentExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 评 </span>
+                              <span class="sidebar-normal"> 评论列表 </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" data-toggle="collapse" href="#messageExamples">
+                    <i class="material-icons">message</i>
+                    <p> 留言管理 
+                    <b class="caret"></b>
+                    </p>
+                </a>
+
+                <div class="collapse" id="messageExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 留 </span>
+                              <span class="sidebar-normal"> 留言列表 </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" data-toggle="collapse" href="#memberExamples">
+                    <i class="material-icons">people_outline</i>
+                    <p> 用户管理 
+                    <b class="caret"></b>
+                    </p>
+                </a>
+
+                <div class="collapse" id="memberExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 用 </span>
+                              <span class="sidebar-normal"> 用户列表 </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" data-toggle="collapse" href="#aboutExamples">
+                    <i class="material-icons">bubble_chart</i>
+                    <p> 关于管理 
+                    <b class="caret"></b>
+                    </p>
+                </a>
+
+                <div class="collapse" id="aboutExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 角 </span>
+                              <span class="sidebar-normal"> 角色管理 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 权 </span>
+                              <span class="sidebar-normal"> 权限管理 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 操 </span>
+                              <span class="sidebar-normal"> 操作记录 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 缓 </span>
+                              <span class="sidebar-normal"> 缓存管理 </span>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="../examples/pages/pricing.html">
+                              <span class="sidebar-mini"> 日 </span>
+                              <span class="sidebar-normal"> 日志管理 </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" data-toggle="collapse" href="#systemExamples">
+                    <i class="material-icons">settings_applications</i>
+                    <p> 系统设置 
+                    <b class="caret"></b>
+                    </p>
+                </a>
+
+                <div class="collapse" id="systemExamples">
                     <ul class="nav">
                         <li class="nav-item ">
                             <a class="nav-link" href="../examples/pages/pricing.html">
                               <span class="sidebar-mini"> P </span>
                               <span class="sidebar-normal"> Pricing </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/rtl.html">
-                              <span class="sidebar-mini"> RS </span>
-                              <span class="sidebar-normal"> RTL Support </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/timeline.html">
-                              <span class="sidebar-mini"> T </span>
-                              <span class="sidebar-normal"> Timeline </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/login.html">
-                              <span class="sidebar-mini"> LP </span>
-                              <span class="sidebar-normal"> Login Page </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/register.html">
-                              <span class="sidebar-mini"> RP </span>
-                              <span class="sidebar-normal"> Register Page </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/lock.html">
-                              <span class="sidebar-mini"> LSP </span>
-                              <span class="sidebar-normal"> Lock Screen Page </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link" href="../examples/pages/user.html">
-                              <span class="sidebar-mini"> UP </span>
-                              <span class="sidebar-normal"> User Profile </span>
                             </a>
                         </li>
                     </ul>
